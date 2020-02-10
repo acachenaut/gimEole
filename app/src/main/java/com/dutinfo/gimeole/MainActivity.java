acf5bd5c86a9b3f3cea5ff79160a6f70492e87cc
Aitor Cachenaut <aitor.pilota@yahoo.fr>
2020-02-11 00:17:27 +0100

    Mise en place de deux boutons pour tester les changements de jauge

--- app/src/main/java/com/dutinfo/gimeole/MainActivity.java
+++ app/src/main/java/com/dutinfo/gimeole/MainActivity.java
@@ -2,8 +2,11 @@ package com.dutinfo.gimeole;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.biansemao.widget.ThermometerView;
+import com.sccomponents.gauges.gr008.GR008;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ThermometerView thermometre =findViewById(R.id.thermometre);
+
+        final GR008 jauge = findViewById(R.id.JaugeCirculaire);
+
+        Button boutonTemperatureAlternateur = findViewById(R.id.boutonTemperatureAlternateur);
+        Button boutonVitesseRotation = findViewById(R.id.boutonVitesseDeRotation);
+
+        boutonTemperatureAlternateur.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                jauge.setVisibility(-1);
+                thermometre.setVisibility(1);
+
+            }
+        });
+
+        boutonVitesseRotation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                thermometre.setVisibility(-1);
+                jauge.setVisibility(1);
+
+            }
+        });
 
     }
 }
