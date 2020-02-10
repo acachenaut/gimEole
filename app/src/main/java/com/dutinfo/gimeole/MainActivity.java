c80bf88b65a5e73a597d7ac1231f31de468c9aa0
Aitor Cachenaut <aitor.pilota@yahoo.fr>
2020-02-11 00:01:41 +0100

    Ajout d'un thermomètre à l'interface graphique

--- app/src/main/java/com/dutinfo/gimeole/MainActivity.java
+++ app/src/main/java/com/dutinfo/gimeole/MainActivity.java
@@ -3,6 +3,8 @@ package com.dutinfo.gimeole;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.biansemao.widget.ThermometerView;
+
 
 public class MainActivity extends AppCompatActivity {
 
