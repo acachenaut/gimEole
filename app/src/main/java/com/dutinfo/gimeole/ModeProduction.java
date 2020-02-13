9abd8823ceca2e7cf9c0c4ded2b672905369c72a
Aitor Cachenaut <aitor.pilota@yahoo.fr>
2020-02-13 21:28:20 +0100

    Ajout de 2 méthodes qui permettront de de récupérer les réglages des jauges entre l'activité réglage et l'activité Mode Production

--- app/src/main/java/com/dutinfo/gimeole/ModeProduction.java
+++ app/src/main/java/com/dutinfo/gimeole/ModeProduction.java
@@ -58,4 +58,45 @@ public class ModeProduction {
     public Element_A_Afficher getVitesseRotation() {
         return vitesseRotation;
     }
+
+    public double[] getMinMaxDesJauges() {
+
+        double[] minMax = new double[14];
+        minMax[0]=this.getVitesseRotation().getValMinJauge();
+        minMax[1]=this.getVitesseRotation().getValMaxJauge();
+        minMax[2]=this.getTensionEnEntree().getValMinJauge();
+        minMax[3]=this.getTensionEnEntree().getValMaxJauge();
+        minMax[4]=this.getCourantEnEntree().getValMinJauge();
+        minMax[5]=this.getCourantEnEntree().getValMaxJauge();
+        minMax[6]=this.getPuissanceFournie().getValMinJauge();
+        minMax[7]=this.getPuissanceFournie().getValMaxJauge();
+        minMax[8]=this.getEnergieProduite().getValMinJauge();
+        minMax[9]=this.getEnergieProduite().getValMaxJauge();
+        minMax[10]=this.getTemperatureAlternateur().getValMinJauge();
+        minMax[11]=this.getTemperatureAlternateur().getValMaxJauge();
+        minMax[12]=this.getTemperatureFrein().getValMinJauge();
+        minMax[13]=this.getTemperatureFrein().getValMaxJauge();
+
+        return minMax;
+
+
+    }
+
+    public void setMinMaxDesJauges(double[] tab){
+        this.getVitesseRotation().setValMinJauge(tab[0]);
+        this.getVitesseRotation().setValMaxJauge(tab[1]);
+        this.getTensionEnEntree().setValMinJauge(tab[2]);
+        this.getTensionEnEntree().setValMaxJauge(tab[3]);
+        this.getCourantEnEntree().setValMinJauge(tab[4]);
+        this.getCourantEnEntree().setValMaxJauge(tab[5]);
+        this.getPuissanceFournie().setValMinJauge(tab[6]);
+        this.getPuissanceFournie().setValMaxJauge(tab[7]);
+        this.getEnergieProduite().setValMinJauge(tab[8]);
+        this.getEnergieProduite().setValMaxJauge(tab[9]);
+        this.getTemperatureAlternateur().setValMinJauge(tab[10]);
+        this.getTemperatureAlternateur().setValMaxJauge(tab[11]);
+        this.getTemperatureFrein().setValMinJauge(tab[12]);
+        this.getTemperatureFrein().setValMaxJauge(tab[13]);
+    }
+
 }
