5c8ecd0d87f0b8e3095ef4cf0c6d167da084291d
Aitor Cachenaut <aitor.pilota@yahoo.fr>
2020-02-19 22:31:22 +0100

    Ajout de l'activation du Bluetooth

--- app/src/main/java/com/dutinfo/gimeole/MainActivity.java
+++ app/src/main/java/com/dutinfo/gimeole/MainActivity.java
@@ -1,11 +1,13 @@
 package com.dutinfo.gimeole;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -68,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
 
         //Lien entre le nom de la jauge courante de l'interface et l'activité
         final TextView nomJaugeCourante = findViewById(R.id.nomJaugeCourante);
+
+        BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (blueAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Le terminal ne possède pas le Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        if (!blueAdapter.isEnabled()) {
+            blueAdapter.enable();
+        }
 
 
 
