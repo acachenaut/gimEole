614dfeefd376a77cade4192f32813a0f7bb41971
miloud <miloud.bouazza213@gmail.com>
2020-03-04 16:50:10 +0100

    debut de l'interface du mode test

---
+++ app/src/main/java/com/dutinfo/gimeole/ModeTestActivity.java
@@ -0,0 +1,14 @@
+package com.dutinfo.gimeole;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_test);
+    }
+}
