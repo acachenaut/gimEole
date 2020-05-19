5ffef2123ae68ca9634007717962bc7438d5cb09
Aitor Cachenaut <aitor.pilota@yahoo.fr>
2020-05-19 10:31:31 +0200

    Modification des valeurs affichées en fonction du nombre de chiffres significatifs

--- app/src/main/java/com/dutinfo/gimeole/ClassesUtiles/BoiteAOutils.java
+++ app/src/main/java/com/dutinfo/gimeole/ClassesUtiles/BoiteAOutils.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 
 public class BoiteAOutils {
 
-    public static String obtenirEcritureScientifiqueAvecChiffresSignificatifs(int nombreDeChiffreSignificatif, Double chiffreATransfromer){
+    public static String obtenirEcritureScientifiqueAvecChiffresSignificatifs(int nombreDeChiffresApresLaVirgule, Double chiffreATransfromer){
         String chiffreAAfficher=Double.toString(chiffreATransfromer) ;
         int puissanceDe10 = 0;
         if (chiffreATransfromer<10000000){
@@ -14,7 +14,7 @@ public class BoiteAOutils {
                 chiffreAAfficher=Double.toString(chiffreATransfromer);
             }
             String nbDeChiffreSignificatif="";
-            for (int i = 0 ; i<nombreDeChiffreSignificatif;i++){
+            for (int i = 0 ; i<nombreDeChiffresApresLaVirgule;i++){
                 nbDeChiffreSignificatif+="0";
             }
             DecimalFormat df = new DecimalFormat("0."+nbDeChiffreSignificatif);
@@ -27,5 +27,41 @@ public class BoiteAOutils {
 
         return chiffreAAfficher;
     }
+
+    public static String arrondirChiffreEnFonctionDuNombreDeChiffresSignificatifs(int nombreDeChiffresSignificatifs, Double chiffreATransfromer){
+
+        int nombreDeChiffreAvantLaVirgule;
+        if (chiffreATransfromer < 1){
+            nombreDeChiffreAvantLaVirgule = 0;
+        }
+        else if ( chiffreATransfromer < 10){
+            nombreDeChiffreAvantLaVirgule = 1;
+        }
+        else if ( chiffreATransfromer < 100 ){
+            nombreDeChiffreAvantLaVirgule = 2;
+        }
+        else if ( chiffreATransfromer < 1000 ){
+            nombreDeChiffreAvantLaVirgule = 3;
+        }
+        else{
+            nombreDeChiffreAvantLaVirgule = 4;
+        }
+
+        if ( nombreDeChiffresSignificatifs-nombreDeChiffreAvantLaVirgule == 0 ){
+            int chiffreFinal;
+            chiffreFinal = (int) Math.round(chiffreATransfromer);
+            String chiffreAAfficher= String.valueOf(chiffreFinal);
+            return chiffreAAfficher;
+        }
+        else {
+            Double chiffreFinal;
+            chiffreFinal = Math.round(chiffreATransfromer*Math.pow(10,nombreDeChiffresSignificatifs-nombreDeChiffreAvantLaVirgule))/Math.pow(10,nombreDeChiffresSignificatifs-nombreDeChiffreAvantLaVirgule);
+            String chiffreAAfficher=Double.toString(chiffreFinal) ;
+            return chiffreAAfficher;
+        }
+
+
+    }
+
 
 }
